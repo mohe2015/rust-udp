11a466bb4eef423e1de9cffed8c936e7595c1589
Hedtke, Moritz <Moritz.Hedtke@t-online.de>
2021-04-22 00:04:40 +0200

    Minimal echo server

---
+++ README.md
@@ -0,0 +1,7 @@
+# rust-udp
+
+## Testing
+
+```bash
+nc -u ::1 1337
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::UdpSocket;
+use std::io;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let socket = UdpSocket::bind("[::1]:1337").await?;
+    println!("Listening on [::1]:1337");
+
+    let mut buf = [0; 1024];
+    loop {
+        let (len, addr) = socket.recv_from(&mut buf).await?;
+        println!("{:?} bytes received from {:?}", len, addr);
+
+        let len = socket.send_to(&buf[..len], addr).await?;
+        println!("{:?} bytes sent", len);
+    }
+
+    Ok(())
 }

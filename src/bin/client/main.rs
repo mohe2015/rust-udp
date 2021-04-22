f7685902d1c01619a505f88f256287f5433e88cc
Hedtke, Moritz <Moritz.Hedtke@t-online.de>
2021-04-22 23:36:39 +0200

    performance

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,12 +1,13 @@
 use tokio::net::UdpSocket;
 use std::io;
+use std::net::{SocketAddr};
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let socket = UdpSocket::bind("[::]:1337").await?;
-    println!("Listening on [::]:1337");
+async fn process() -> io::Result<()> {
+    let socket = UdpSocket::bind("[::]:0").await?;
+    println!("Listening on [::]:1338");
 
-    let addr = "[2003:c0:470d:e2c8:8a3:837b:5f1b:73d9]:1337";
+    //let addr = "[2003:c0:470d:e2c8:8a3:837b:5f1b:73d9]:1337".parse::<SocketAddr>().unwrap();
+    let addr = "[2003:c0:470d:e2c8:78a4:681c:1c74:3a7a]:1337".parse::<SocketAddr>().unwrap();
     socket.connect(addr).await?;
     let mut buf = [0; 8192];
     loop {
@@ -16,6 +17,21 @@ async fn main() -> io::Result<()> {
         let (len, addr) = socket.recv_from(&mut buf).await?;
         //println!("{:?} bytes received from {:?}", len, addr);
     }
+
+}
+
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut handles = vec![];
+    for _ in 0..8 {
+        println!("starting a process");
+        handles.push(tokio::spawn(async move {
+            // Process each socket concurrently.
+            process().await
+        }));
+    }
+    futures::future::join_all(handles).await;
 
     Ok(())
 }
--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,9 +1,20 @@
 use tokio::net::UdpSocket;
 use std::io;
+use socket2::{Socket, Domain, Type};
+use std::net::{SocketAddr};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let socket = UdpSocket::bind("[::]:1337").await?;
+    let address: SocketAddr = "[::]:1337".parse().unwrap();
+    let address = address.into();
+
+    let socket2_socket = Socket::new(Domain::IPV6, Type::DGRAM, None)?;
+
+    socket2_socket.bind(&address)?;
+    socket2_socket.set_nonblocking(true)?;
+    socket2_socket.set_reuse_address(true)?;
+
+    let socket = UdpSocket::from_std(socket2_socket.into())?;
     println!("Listening on [::]:1337");
 
     let mut buf = [0; 8192];

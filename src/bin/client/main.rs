10ba1bd0e74cd596eaefe82970add6c8e28250b5
Hedtke, Moritz <Moritz.Hedtke@t-online.de>
2021-04-22 22:56:30 +0200

    basic client

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -6,14 +6,15 @@ async fn main() -> io::Result<()> {
     let socket = UdpSocket::bind("[::]:1337").await?;
     println!("Listening on [::]:1337");
 
-    socket.connect("192.168.2.121:1337").await?;
+    let addr = "[2003:c0:470d:e2c8:8a3:837b:5f1b:73d9]:1337";
+    socket.connect(addr).await?;
     let mut buf = [0; 1024];
     loop {
+        let len = socket.send_to(&buf, addr).await?;
+        println!("{:?} bytes sent", len);
+
         let (len, addr) = socket.recv_from(&mut buf).await?;
         println!("{:?} bytes received from {:?}", len, addr);
-
-        let len = socket.send_to(&buf[..len], addr).await?;
-        println!("{:?} bytes sent", len);
     }
 
     Ok(())
